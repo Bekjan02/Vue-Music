f9538b9b6cdb6483a4528866e32327e60ebbc836
Bekjan02 <bayganaliyev@mail.ru>
2021-07-29 18:15:25 +0600

    /

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,35 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home";
+import Login from "../views/auth/Login";
+import Signup from "../views/auth/Signup";
+import CreatePlaylist from "../views/playlists/CreatePlaylist";
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    {
+        path: "/",
+        name: "Home",
+        component: Home,
+    },
+    {
+        path: "/login",
+        name: "Login",
+        component: Login,
+    },
+    {
+        path: "/signup",
+        name: "Signup",
+        component: Signup,
+    },
+    {
+        path: "/playlist/create",
+        name: "CreatePlaylist",
+        component: CreatePlaylist,
+    },
+];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
+});
 
-export default router
+export default router;
---
+++ src/views/playlists/CreatePlaylist.vue
@@ -0,0 +1,71 @@
+<template>
+    <form @submit.prevent="handleSubmit">
+        <h4>Create New Playlist</h4>
+        <input type="text" placeholder="Plalist" required v-model="title" />
+        <textarea
+            placeholder="Playlist description"
+            required
+            v-model="description"
+        ></textarea>
+        <label for="">Upload playlist cover image</label>
+        <input type="file" @change="handleChange" />
+        <div class="error">{{ fileError }} </div>
+        <button>
+            Create
+        </button>
+    </form>
+</template>
+
+<script>
+import { ref } from "@vue/reactivity";
+export default {
+    setup() {
+        const title = ref("");
+        const description = ref("");
+        const file = ref("");
+        const fileError = ref('')
+
+        const imgTypes = ["image/jpeg", "image/jpg", "image/png"];
+
+        const handleSubmit = () => {
+            if(file.value) {
+                console.log(title.value, description.value, file.value);
+            }
+        };
+
+        const handleChange = (event) => {
+            const selectedFile = event.target.files[0];
+            if (selectedFile && imgTypes.includes(selectedFile.type)) {
+                file.value = selectedFile;
+                fileError.value = null
+            } else {
+                fileError.value = ' Пожалуйста выберите картинку (png/jpeg/jph) '
+                file.value = null;
+            }
+        };
+
+        return { title, description, fileError, handleSubmit, handleChange };
+    },
+};
+</script>
+
+<style>
+form {
+    background: white;
+}
+input[type="file"] {
+    border: 0;
+    padding: 0;
+}
+textarea {
+    resize: none;
+}
+label {
+    font-size: 12px;
+    display: block;
+    margin-top: 30px;
+}
+button {
+    margin-top: 20px;
+}
+</style>
